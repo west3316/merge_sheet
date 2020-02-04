459a052a33a25bccd9b43666eeb3e46d5f4fb52b
GodanKing <west3316@qq.com>
2020-02-04 15:06:38 +0800

    增加重名处理，增加选项s

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"flag"
-	"io/ioutil"
 	"fmt"
+	"io/ioutil"
 	"path/filepath"
+	"strconv"
 
 	"github.com/tealeg/xlsx"
 )
@@ -22,38 +23,56 @@ func getXlsxFiles(dir string) []xlsxFile {
 
 	var slice []xlsxFile
 	for _, file := range files {
-		if  filepath.Ext(file.Name()) != ".xlsx"{
+		if filepath.Ext(file.Name()) != ".xlsx" {
 			continue
 		}
-		wb, err :=	xlsx.OpenFile(filepath.Join(dir, file.Name()))
+		wb, err := xlsx.OpenFile(filepath.Join(dir, file.Name()))
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
 
-		slice = append(slice, xlsxFile{Filename: file.Name(), Workbook:wb })
+		slice = append(slice, xlsxFile{Filename: file.Name(), Workbook: wb})
 	}
 	return slice
 }
 
 func main() {
-	// var files string
 	var out string
 	var dir string
+	var single bool
 	flag.StringVar(&dir, "d", "", "指定需要合并的工作薄目录")
-	// flag.StringVar(&files, "i", "", "指定需要合并的工作薄，格式：[wb1.xlsx, wb2.xlsx]")
 	flag.StringVar(&out, "o", "out.xlsx", "指定合并后文件的命名，格式：out.xlsx")
+	flag.BoolVar(&single, "s", false, "使用文件名，命名单sheet工作薄")
 	flag.Parse()
 
-	
-	wb:= xlsx.NewFile()
+	wb := xlsx.NewFile()
 
+	unique := make(map[string]int)
 	xfiles := getXlsxFiles(dir)
 	for _, xf := range xfiles {
 		fmt.Println("发现xlsx文件", xf.Filename)
+		var count int
+		var sheetName string
 		for _, sheet := range xf.Workbook.Sheets {
-			wb.AppendSheet(*sheet, sheet.Name)
-			fmt.Println("增加sheet", sheet.Name)
+			if single && len(xf.Workbook.Sheets) == 1 {
+				_, f := filepath.Split(xf.Filename)
+				sheetName = f[:len(f)-len(filepath.Ext(xf.Filename))]
+			} else {
+				count = unique[sheet.Name]
+				sheetName = sheet.Name
+				if count > 0 {
+					sheetName += " (" + strconv.Itoa(count) + ")"
+				}
+			}
+
+			wb.AppendSheet(*sheet, sheetName)
+			unique[sheet.Name]++
+			if count > 0 {
+				fmt.Println("增加sheet", sheet.Name, " => ", sheetName)
+			} else {
+				fmt.Println("增加sheet", sheet.Name)
+			}
 		}
 	}
 
